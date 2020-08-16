5730cc7dc865118b97c08611fe5d9f8b4091c1e4
Yaroslav Stolbunov <47113131+Yaroher2442@users.noreply.github.com>
2020-08-16 17:25:24 +0300

    add back page to adding

--- API.py
+++ API.py
@@ -49,8 +49,10 @@ class API(object):
 	def log():
 		if request.method == 'GET':
 			get_db()
-			print(g._database)
-			return render_template('auth.html')
+			if request.cookies.get('user_id') == None:
+				return render_template('auth.html')
+			else:
+				return redirect('/sud_dela')
 		else:
 			return abort(401)
 
@@ -61,6 +63,14 @@ class API(object):
 		else:
 			print(request.cookies.get('user_id'))
 			return render_template("index_admin.html")
+
+
+	@flask_app.route('/add_delo', methods=['GET', 'POST'])
+	def add_delo():
+		if request.method == 'POST':
+			pass
+		else:
+			return render_template("add_delo.html")
 #---------------------------------------------------------------------
 	@flask_app.route('/sud_dela', methods=['GET', 'POST'])
 	def sud_dela():
@@ -79,7 +89,7 @@ class API(object):
 				data=d_table,
 				role=role,
 				name=name,
-				urists=[1,1,1])
+				urists=[1,2,3])
 #----------------------------------------------------------------------
 	@flask_app.route('/bank_dela', methods=['GET', 'POST'])
 	def bank_dela():
